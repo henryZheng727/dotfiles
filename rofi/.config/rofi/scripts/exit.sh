235ef23860e004e76b0a91d864b6852cf131e6f3
Henry Zheng <henry.zheng727@gmail.com>
2025-08-01 15:06:12 -0600

    barebones rofi/sway setup

---
+++ rofi/.config/rofi/scripts/exit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script allows you to shut down, reboot, or log out.
+
+case "$@" in
+    "Power Off")
+        shutdown now
+        ;;
+    "Reboot")
+        reboot
+        ;;
+    "Log Out")
+        swaymsg exit
+        # swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' 'swaymsg exit'
+        ;;
+    "Cancel")
+        exit
+        ;;
+esac
+
+echo "Power Off"
+echo "Reboot"
+echo "Log Out"
+echo "Cancel"
---
+++ sway/README.md
@@ -0,0 +1,16 @@
+# About
+These are dotfiles for the Sway window manager.
+
+Use them by typing
+```sh
+stow -t ~ sway rofi
+```
+
+They depend on `rofi-wayland` and `sway`.
+
+**You must use these with the rofi dotfiles.**
+
+Some of the functionality relies on calling custom rofi scripts.
+
+# Keybindings
+TODO
