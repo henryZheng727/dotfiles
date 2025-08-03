398f063ecc1d538464a95eba9e942dcbbaaaae10
Henry Zheng <henry.zheng727@gmail.com>
2025-08-03 16:43:11 -0600

    add shell aliases, history; accordingly update script

--- fedora_sway_setup.sh
+++ fedora_sway_setup.sh
@@ -12,53 +12,71 @@ printf "\
 defaultyes=true\n\
 max_parallel_downloads=10\n\
 color=always\n\
-" | sudo tee /etc/dnf/dnf.conf || exit
-echo "Fixed DNF!"
+" | sudo tee /etc/dnf/dnf.conf
+printf "Fixed DNF!"
 
 # make your battery not suck
-sudo dnf install tlp || exit
-systemctl enable tlp.service || exit
+sudo dnf install tlp -y || exit
+# systemctl enable tlp.service || exit
 echo "Enabled TLP!"
 
-# download and install dotfiles for all major non-flatpak apps
-# TODO - set up zsh and maybe starship?
-sudo dnf install git stow fastfetch neovim ranger kitty pandoc -y || exit
+# copy dotfiles
+sudo dnf install git stow || exit
 cd ~ || exit
-mkdir software
+mkdir software || exit
 cd software || exit
 git clone https://github.com/henryzheng727/dotfiles.git || exit
 cd dotfiles || exit
-stow -t ~ nvim kitty || exit
-echo "Stowed dotfiles and installed terminal apps!"
+
+# download and set up terminal
+sudo dnf install \
+    zsh \
+    zsh-autosuggestions \
+    zsh-syntax-highlighting \
+    kitty \
+    fastfetch \
+    figlet \
+    -y || exit
+chsh -s "$(which zsh)" || exit
+stow -t ~ kitty zsh || exit
+
+# download major terminal apps
+sudo dnf install neovim ranger pandoc -y || exit
+
+# install dotfiles for rofi, sway
+stow -t ~ sway rofi || exit
 
 # download flatpaks that i use
 sudo dnf install flatpak || exit
 flatpak remote-add --if-not-exists flathub \
-    https://dl.flathub.org/repo/flathub.flatpakrepo || exit
+    https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub \
     app.zen_browser.zen \
-    com.calibre_ebook.calibre \
-    com.github.jeromerobert.pdfarranger \
-    com.github.tchx84.Flatseal \
-    com.google.Chrome \
-    com.obsproject.Studio \
-    com.spotify.Client \
-    com.vscodium.codium \
-    dev.vencord.Vesktop \
-    fr.handbrake.ghb \
-    md.obsidian.Obsidian \
-    org.audacityteam.Audacity \
-    org.gimp.GIMP \
-    org.kde.kdenlive \
-    org.kde.krita \
-    org.libreoffice.LibreOffice \
-    org.videolan.VLC \
-    us.zoom.Zoom \
-    com.valvesoftware.Steam || exit
-echo "Installed important Flatpak apps!"
+#     com.calibre_ebook.calibre \
+#     com.github.jeromerobert.pdfarranger \
+#     com.github.tchx84.Flatseal \
+#     com.google.Chrome \
+#     com.obsproject.Studio \
+#     com.spotify.Client \
+#     com.vscodium.codium \
+#     dev.vencord.Vesktop \
+#     fr.handbrake.ghb \
+#     md.obsidian.Obsidian \
+#     org.audacityteam.Audacity \
+#     org.gimp.GIMP \
+#     org.kde.kdenlive \
+#     org.kde.krita \
+#     org.libreoffice.LibreOffice \
+#     org.videolan.VLC \
+#     us.zoom.Zoom \
+#     com.valvesoftware.Steam || exit
+# echo "Installed important Flatpak apps!"
 
 # todo - find some way to backup and restore ~
 # pika backup or borg may be useful. borg with rsync would be nice
 
 # remove unnecessary apps
 sudo dnf remove firefox foot
+
+# launch a new terminal
+kitty
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,9 +1,35 @@
 # Environment Variables
+# XDG base dir specification
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+# default programs
 export EDITOR=nvim
+
+# History
+HISTFILE=$XDG_CACHE_HOME/zsh/history
+HISTSIZE=100000
+SAVEHIST=100000
+setopt HIST_IGNORE_SPACE # ignore commands starting w/ space
+setopt HIST_IGNORE_DUPS # ignore duplicate commands
+setopt SHARE_HISTORY # share history. i don't use tmux, so maybe unnecessary
 
 # Aliases
 # improved ls, if it exists
-{ which lsd &> /dev/null } && alias ls=lsd
+( which lsd &> /dev/null ) && alias ls=lsd
+# vim exit lmao
+alias :q=exit
+alias :wq=exit
+alias q=exit
+alias wq=exit
+# quickly enter classes
+CLASSES=(cs3700 cs4000 cs4400 cs6160 cs6520 math5600)
+SEMESTER=fa26
+for CLASS in $CLASSES;
+do
+    CLASSPATH="${HOME}/university/${SEMESTER}/${CLASS}";
+    test -d "${CLASSPATH}" && alias $CLASS="cd ${CLASSPATH}"
+done;
 
 # Autoload
 autoload -U colors && colors
@@ -12,8 +38,8 @@ autoload -U colors && colors
 autoload -U compinit
 zstyle ':completion:*' menu select
 zmodload zsh/complist
-compinit
 _comp_options+=(globdots)
+compinit
 
 # Prompt
 PROMPT_DATE='[ %F{magenta}%@ %W%f ]'
@@ -29,8 +55,8 @@ setopt CORRECT
 bindkey -v
 
 # Aura farming
-{ which fastfetch &> /dev/null } && fastfetch
-{ which figlet &> /dev/null } && print -P "%F{magenta}$(figlet $USERNAME)%f"
+( which fastfetch &> /dev/null ) && fastfetch
+( which figlet &> /dev/null ) && print -P "%F{magenta}$(figlet $USERNAME)%f"
 
 # Syntax Highlighting and Autosuggestions
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh \
