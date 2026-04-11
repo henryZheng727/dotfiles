d2fdc17001ee5f6757f0d9e092a709619f30f623
Henry Zheng <henry.zheng727@gmail.com>
2026-04-11 13:45:57 -0600

    add function to quickly clone a github repo

---
+++ fish/functions/gclone.fish
@@ -0,0 +1,21 @@
+function gclone
+    if test (count $argv) -lt 1 -o (count $argv) -gt 2
+        echo "gclone: usage: gclone <author/repo> [dir_name]" >&2
+        return 1
+    end
+
+    set repo $argv[1]
+
+    if not string match -qr '^[A-Za-z0-9_.-]+/[A-Za-z0-9_.-]+$' -- $repo
+        echo "gclone: invalid format, expected <author/repo>" >&2
+        return 1
+    end
+
+    set url "https://github.com/$repo"
+
+    if test (count $argv) -eq 2
+        git clone -- $url $argv[2]
+    else
+        git clone -- $url
+    end
+end
